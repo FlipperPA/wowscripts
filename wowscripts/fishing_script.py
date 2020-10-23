174b8036d52f0a0c1460eae3e171df76de2251e2
Brandon Lafving <52582453+blafving@users.noreply.github.com>
2020-10-23 11:21:10 -0400

    Updating fishing script for easier customization
    I pulled out some of the variables into global variables at top so you can easily control casts, image file, and confidence.

--- wowscripts/fishing_script.py
+++ wowscripts/fishing_script.py
@@ -1,12 +1,16 @@
 import pyautogui as gui
 import cv2
 
+IMAGE_FILE = 'wowscripts/lure_vale.png'
+CASTS = 30
+CONFIDENCE = .6
+GRAYSCALE = True
 
 def cast():
     """
     Casts the line and returns the box region of the lure
     """
-    cast_icon = gui.locateCenterOnScreen('wow/gofish_icon.png')
+    cast_icon = gui.locateCenterOnScreen('wowscripts/gofish_icon.png')
     gui.moveTo(cast_icon)
     gui.click()
     gui.PAUSE = 2.0
@@ -16,22 +20,26 @@ def catch(image):
     """
     takes a locateOnScreen object and clicks when the lure drops
     """
-    location = gui.locateOnScreen(image, confidence=0.5)
-    while location == None:
-        location = gui.locateOnScreen(image, confidence=0.5)
+    location = gui.locateOnScreen(image, confidence=CONFIDENCE, grayscale = GRAYSCALE)
+    if location == None:
+        return
     print(location)
     counter = 30
-    while gui.locateOnScreen(image, region=location, confidence=0.5) and counter > 0:
+    while gui.locateOnScreen(image, region=location, confidence=CONFIDENCE - .1, grayscale=GRAYSCALE) and counter > 0:
         print('no fish yet')
         counter -= 1
     gui.click( x=(location[0] + 40), y=(location[1] + 25) )
+    if counter == 0:
+        print('missed one!')
     return
 
 def fish(image):
-    counter = 10
+    counter = CASTS
     while counter > 0:
+        print('Cast {}'.format(counter))
         cast()
         catch(image)
         counter -= 1
 
-fish('wow/lure_front')
+fish(IMAGE_FILE)
+
